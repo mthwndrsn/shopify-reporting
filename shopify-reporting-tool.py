d611c9629b6aa2c9ebdbc3f35fe60989ee4ef0ae
Matt <mattanderson@me.com>
2022-04-25 15:58:10 +1000

    Change to f-strings

--- shopify-reporting-tool.py
+++ shopify-reporting-tool.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import numpy as np
 
+###
+## import data
+###
 
 orders = pd.read_csv(r'C:\Users\Matt\data\shopify\orders_export_1.csv')
 customers = pd.read_csv(r'C:\Users\Matt\data\shopify\customers_export_1.csv')
 # import mailchimp data and join on email
 
+###
+# merge data sets
+###
 
+###
+# Clean up data
+###
+orders['Paid at'] = pd.to_datetime(orders['Paid at'])
+
+
+# setup seperate column for year
+# setup seperate column for month
+
+###
 # key metric calcs
+###
 aov = orders['Total'].mean()
 revenue = orders['Total'].sum()
 # order count =
@@ -20,8 +37,8 @@ groupby_month = orders.groupby('Paid at').sum()
 
 
 # Order Metrics
-print('Average Order Value = $' + str(aov))
-print('Total store revenue = $' + str(revenue))
-print('Total customers = ' + str(total_customers))
+print(f'Average Order Value = ${aov}')
+print(f'Total store revenue = ${revenue}')
+print(f'Total customers = {total_customers}')
 
 print('Grouped by Month' + str(groupby_month))
